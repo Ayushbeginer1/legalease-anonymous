96e249b746376068dd8c3c2f09408ff88f3a4ecf
Ayushbeginer1 <mahatoayush262@gmail.com>
2025-07-01 16:11:28 +0530

    ..

--- first/urls.py
+++ first/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import submit_anonymous_tip, list_public_tips, show_all_tips, download_evidence
+from django.http import HttpResponse
 
 urlpatterns = [
     path('submit-tips/', submit_anonymous_tip),
     path('tips/', list_public_tips),
     path('tips-table/', show_all_tips, name='tips_table'),
     path('download-evidence/<uuid:access_key>/',download_evidence, name="download_evidence"),
+    path('',lambda request: HttpResponse("CONNECT TO FRONTEND"))
 ]
